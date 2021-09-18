c15553959769ecdf2e09b935a97669f9f5c95fe7
sudjoao <isudjoao@gmail.com>
2021-09-18 10:16:57 -0300

    Add alert trying to add task with the same name, and trying to remove a task

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { Alert, StyleSheet, View } from 'react-native';
 
 import { Header } from '../components/Header';
 import { Task, TasksList } from '../components/TasksList';
@@ -9,6 +9,11 @@ export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
+    const taskAlreadyExists = tasks.find(task => task.title == newTaskTitle)
+    if (taskAlreadyExists) {
+      Alert.alert("Task já cadastrada.", "Você não pode cadastrar uma task com o mesmo nome");
+      return;
+    }
     const newTask = {
       id: new Date().getTime(),
       title: newTaskTitle,
@@ -27,9 +32,22 @@ export function Home() {
   }
 
   function handleRemoveTask(id: number) {
-    setTasks(oldTasks => oldTasks.filter(
-      task => task.id != id
-    ))
+    Alert.alert(
+      "Remover item",
+      "Tem certeza que você deseja remover esse item?",
+      [
+        {
+          text: "Não"
+        },
+        {
+          text: "Sim",
+          onPress: () => setTasks(oldTasks => oldTasks.filter(
+            task => task.id != id
+          ))
+        }
+      ]
+    );
+
   }
 
   return (
